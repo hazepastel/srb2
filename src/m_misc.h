c58ec93039a54b14ff140eef90518bb4cb482ad3
pastel <hazepastel@proton.me>
2025-02-01 11:26:27 -0600

    change internal version stuff

--- src/m_misc.h
+++ src/m_misc.h
@@ -40,7 +40,7 @@ void M_SaveFrame(void);
 void M_StopMovie(void);
 
 // the file where game vars and settings are saved
-#define CONFIGFILENAME "config.cfg"
+#define CONFIGFILENAME "redberries.cfg"
 
 INT32 M_MapNumber(char first, char second);
 
